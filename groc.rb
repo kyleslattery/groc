f1c365cfe1a1f89f2635d4a09c5b95ec0eeee6a2
Kyle Slattery <kyle@kyleslattery.com>
2010-02-24 23:20:03 -0500

    Add directory specs

--- groc.rb
+++ groc.rb
@@ -5,10 +5,20 @@ Dir[File.join(File.dirname(__FILE__), "lib", "*.rb")].each do |path|
   require path
 end
 
+# For pages
 get '*' do |path|
   begin
     Groc::Page.new(path).body
   rescue Groc::PathNotFound
     pass
   end
+end
+
+# For directory
+get '*' do |path|
+  begin
+    Groc::Directory.new(path).to_s
+  rescue Groc::PathNotFound
+    pass
+  end  
 end
--- spec/groc_spec.rb
+++ spec/groc_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "Groc: GET /some/path" do
+describe "Groc: GET file" do
   include AppHelper
   
   before(:each) do
@@ -15,11 +15,33 @@ describe "Groc: GET /some/path" do
   
   it "should return 404 if Groc::Page.new raises PathNotFound error" do
     Groc::Page.stub!(:new).and_raise(Groc::PathNotFound)
+    Groc::Directory.stub!(:new).and_raise(Groc::PathNotFound)
     get '/some/path'
   end
   
   it "should output body" do
     get '/some/path'
     last_response.body.should == 'html'
+  end
+end
+
+describe "Groc: GET directory" do
+  include AppHelper
+  
+  before(:each) do
+    Groc::Page.stub!(:new).and_raise(Groc::PathNotFound)
+    
+    @directory_mock = mock(Groc::Directory)
+    Groc::Directory.stub!(:new).and_return(@directory_mock)
+  end
+  
+  it "should call Directory.new with the path" do
+    Groc::Directory.should_receive(:new).with("/some/path").and_return(@directory_mock)
+    get '/some/path'
+  end
+  
+  it "should return 404 if Groc::Directory.new raise PathNotFound error" do
+    Groc::Directory.stub!(:new).and_raise(Groc::PathNotFound)
+    get '/some/path'
   end
 end
